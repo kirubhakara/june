92b7482c95909293d43a45ee2117ad1230a24e0c
kirubakaaran <karankiru1011@gmail.com>
2023-06-20 21:44:06 +0530

    insta

--- src/org/sample/Gitsample.java
+++ src/org/sample/Gitsample.java
@@ -1,6 +1,10 @@
 package org.sample;
 
 public class Gitsample {
+	private void insta() {
+		System.out.println("instagram login");
+
+	}
 public static void main(String[] args) {
 	System.out.println("this my code");
 }
